3707108b9d8a67dfc3a50d2215b89238bb4b1fec
jesus romaldo sulla sacsi <1673202o@senati.pe>
2026-06-05 20:45:23 -0500

    tarea04 agregando métodos al ProductoRepository

--- models/ProductoRepository.php
+++ models/ProductoRepository.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
-require_once __DIR__ . '/../models/Producto.php';
+require_once __DIR__ . '/Producto.php';
 require_once __DIR__ . '/../config/conexion.php';
 
 class ProductoRepository {
